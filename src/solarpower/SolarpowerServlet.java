bfe26100de92347e18a951ac1d143e1821055280
apan0608 <apan0608@hotmail.com>
2012-08-25 17:40:35 +1000

    form

---
+++ src/solarpower/SolarpowerServlet.java
@@ -0,0 +1,43 @@
+package solarpower;
+
+import com.google.appengine.api.datastore.DatastoreFailureException;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import java.util.Date;
+import java.io.IOException;
+
+import java.util.logging.Logger;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tools.ant.taskdefs.XSLTProcess.Factory;
+
+
+public class SolarpowerServlet extends HttpServlet implements Servlet {
+	
+//private static String CALCULATE = "/calculate.jsp";
+private static String HOME ="/home.jsp";
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+	
+//		if(req.getParameterMap().containsKey("calculate")){
+//            resp.sendRedirect(CALCULATE);
+//		} else {
+			resp.sendRedirect(HOME);
+//		}
+        
+    }
+}
---
+++ war/home.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+  <head>
+    <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+  </head>
+<body>
+    <a href="/calculate.jsp">go to claculate page</a>
+
+
+
+ </body>
+</html>
