e92f4210270c3980765cff04690e63edf7558990
cedou68 <cedric.viney@gmail.com>
2012-09-10 13:39:03 +1000

    collections update

--- src/solarpower/servlets/SolarpowerServlet.java
+++ src/solarpower/servlets/SolarpowerServlet.java
@@ -45,6 +45,58 @@ public class SolarpowerServlet extends HttpServlet implements Servlet {
         // }
         
     }
+
+// added this but i don't know if we're still gonna use it?
+/*    // build collections for form
+    public static LinkedHashMap<String, Double> BuildSystemSizes() {
+        LinkedHashMap<String, Double> systemSizes = new LinkedHashMap<String, Double>();
+        
+        systemSizes.put("1kW", 1.0);
+        systemSizes.put("1.5kW", 1.5);
+        systemSizes.put("2kW", 2.0);
+        systemSizes.put("2.5kW", 2.5);
+        systemSizes.put("3kW", 3.0);
+        systemSizes.put("3.5kW", 3.5);
+        systemSizes.put("4kW", 4.0);
+        systemSizes.put("4.5kW", 4.5);
+        systemSizes.put("5kW", 5.0);
+        systemSizes.put("5.5kW", 5.5);
+        systemSizes.put("6kW", 6.0);
+        
+        return systemSizes;
+    }
+    
+    public static LinkedHashMap<String, Integer> BuildNoOfPanels() {
+        LinkedHashMap<String, Integer> noOfPanels = new LinkedHashMap<String, Integer>();
+        
+        noOfPanels.put("4", 4);
+        noOfPanels.put("6", 6);
+        noOfPanels.put("8", 8);
+        noOfPanels.put("10", 10);
+        noOfPanels.put("12", 12);      
+        noOfPanels.put("14", 14);
+        noOfPanels.put("16", 16);
+        noOfPanels.put("18", 18);
+        noOfPanels.put("20", 20);
+        
+        return noOfPanels;
+    }
+    
+    public static LinkedHashMap<String, Integer> BuildPanelsOrientation() {
+        LinkedHashMap<String, Integer> panelsOrientation = new LinkedHashMap<String, Integer>();
+    	
+    	panelsOrientation.put("20 degrees", 20); 
+    	panelsOrientation.put("25 degrees", 25); 
+    	panelsOrientation.put("30 degrees", 30); 
+    	panelsOrientation.put("35 degrees", 35); 
+    	panelsOrientation.put("40 degrees", 40); 
+    	panelsOrientation.put("45 degrees", 45); 
+    	panelsOrientation.put("50 degrees", 50); 
+    	panelsOrientation.put("55 degrees", 55); 
+    	panelsOrientation.put("60 degrees", 60); 
+    	
+    	return panelsOrientation;
+    }*/
     
     
 }
